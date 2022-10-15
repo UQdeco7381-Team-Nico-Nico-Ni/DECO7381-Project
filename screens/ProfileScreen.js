a06379a3e1c202435799c2ee34dc09220993a826
Ellen Wang <ellenwang918@gmail.com>
2022-10-15 23:25:23 +1000

    edit ProfileScreen.js

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,5 +1,12 @@
 import { useState, useEffect, useContext } from "react";
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  Image,
+  ImageBackground,
+} from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 import { Colors } from "../constants/styles";
@@ -67,37 +74,46 @@ const ProfileScreen = (props) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <View style={styles.iconContainer}>
-          <EntypoIconButton
-            icon="arrow-with-circle-left"
-            size={36}
-            color={Colors.bigStone}
+      <ImageBackground
+        source={require("../assets/images/backgroundImage/profile.png")}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.headerContainer}>
+          <View style={styles.iconContainer}>
+            <EntypoIconButton
+              icon="arrow-with-circle-left"
+              size={36}
+              color={Colors.bigStone}
+              onPress={() => {
+                navigation.navigate("MainMenu");
+              }}
+            />
+          </View>
+          <View style={styles.title}>
+            <Text style={styles.headerFont}>My Profile</Text>
+          </View>
+        </View>
+        <View style={styles.profile}>
+          <Image
+            source={require("../assets/images/avatar/avatar.png")}
+            style={styles.image}
+          ></Image>
+          <Text style={styles.profile}>{userName}</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={styles.buttonPink}
             onPress={() => {
-              navigation.navigate("MainMenu");
+              navigation.navigate("ResetPassword");
             }}
-          />
+          >
+            <Text style={styles.buttonFont}>Change Password</Text>
+          </Pressable>
+          <Pressable style={styles.buttonGreen} onPress={authCtx.logout}>
+            <Text style={styles.buttonFont}>Log out</Text>
+          </Pressable>
         </View>
-        <View style={styles.title}>
-          <Text style={styles.headerFont}>My Profile</Text>
-        </View>
-      </View>
-      <View style={styles.profile}>
-        <Text>User Name: {userName}</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Pressable
-          style={styles.buttonPink}
-          onPress={() => {
-            navigation.navigate("ResetPassword");
-          }}
-        >
-          <Text style={styles.buttonFont}>Change Password</Text>
-        </Pressable>
-        <Pressable style={styles.buttonGreen} onPress={authCtx.logout}>
-          <Text style={styles.buttonFont}>Log out</Text>
-        </Pressable>
-      </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -105,26 +121,31 @@ const ProfileScreen = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40,
+    marginTop: 40,
   },
   buttonContainer: {
-    flex: 4,
+    flex: 3,
     alignItems: "center",
+    marginBottom: 100,
   },
   headerContainer: {
     flex: 1,
     flexDirection: "row",
+
   },
   title: {
     paddingTop: 10,
     paddingLeft: 15,
   },
   profile: {
-    flex: 1,
+    flex: 2,
     alignItems: "center",
+    color: Colors.elephantEar,
+    fontSize: 15,
+    fontWeight: "bold",
   },
   buttonPink: {
-    width: 200,
+    width: 280,
     height: 55,
     backgroundColor: Colors.bashfulness,
     borderWidth: 2,
@@ -150,7 +171,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   buttonGreen: {
-    width: 200,
+    width: 280,
     height: 55,
     backgroundColor: Colors.pineTree,
     borderWidth: 2,
@@ -171,6 +192,16 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: Colors.bigStone,
+  },
+  image: {
+    height: 120,
+    width: 120,
+    marginBottom: 20,
+    resizeMode: "contain",
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
   },
 });
 
